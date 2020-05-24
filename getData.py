3aeac5756fc313e20f6dd6d6fa8dfa7369339ce1
SimonTheWang <simonwg2001@gmail.com>
2020-05-23 20:22:38 -0400

    created the request folder with functions getData and parseData, also included a dependency file along with a comments file

---
+++ getData.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+def getData(country, status, fromUTC, toUTC):
+    country = 'country/' + country;
+    status = '/status/' + status
+    parameters = {
+        "from" : fromUTC,
+        "to" : toUTC,
+    }
+
+    response = requests.get("https://api.covid19api.com/"+country+status,parameters)
+    data = response.json()
+    return data
+
+def parseData(data,province = '',city= ''):
+    days = []
+    cases = []
+    count = 0
+    for day in range(len(data)):
+        if (province == data[day]['Province'] or not province) and (city == data[day]['City'] or not city):
+            cases.append(data[day]['Cases'])
+            count+=1
+            days.append(count)
+    print(cases)
+    print(days)
+    return
+
+
+
+
+
+
+
+
+x = getData("Canada", "confirmed", "2020-03-01T00:00:00Z", "2020-03-17T00:00:00Z")
+parseData(x, 'Quebec')
