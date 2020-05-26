0e2dec4d249413b3bea8b1ba939875da3d873bb2
SimonTheWang <simonwg2001@gmail.com>
2020-05-25 23:11:07 -0400

    made the code very slow omegalul

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,16 +3,18 @@ from getData import *
 
 import matplotlib.pyplot as plt
 
-
+print('available countries are: ' + ', '.join(getCountries()) )
 country = input('Select a country: ')
-city = input('If applicable, select a city: ')
-
-province = input('If applicable, select a province: ')
 status = input('Enter Status number (1 : (confirmed) , 2 : (recovered) , 3 : (deaths): ')
+possibleProvinceCity = (searchProvinceCity(getData(country,status)))
+procity = (', '.join(list(possibleProvinceCity.keys())))
+print('available '+ procity + 's are ' + (', '.join(list(possibleProvinceCity[procity]))))
+provincecity = input('If applicable, select a city or province: ')
 
-data = parseData(getData(country,status), province, city)
 
 
+data = parseData(getData(country,status),(possibleProvinceCity),provincecity)
+print
 #sets the data
 x = data['days']
 y1 = data['totalCases']
@@ -30,7 +32,7 @@ ax.plot(x, y2,'--', color='tab:orange', label = 'predicted cases per day')
 ax.set_xlabel('number of days')
 ax.set_ylabel(('cases of status : ' + data['type']))
 ax.legend()
-ax.set_title('# of cases of status: ' + data['type'] + ', with respect to time, starting from ' + data['day1'])
+ax.set_title('# of cases of status: ' + data['type'] + ', with respect to time, starting from ' + data['day1'] +' in '+ data['provincecity'] + ', ' + data['country'])
 
 # display the plot
 plt.show()
--- frontend/getData.py
+++ frontend/getData.py
@@ -3,8 +3,17 @@ import requests
 import json
 
 APIreference = ' refer to https://documenter.getpostman.com/view/10808728/SzS8rjbc?version=latest for more info'
+URL = "https://api.covid19api.com/"
 
-def getData(country, status = 'confirmed', fromUTC = "2020-03-01T00:00:00Z", toUTC = datetime.utcnow()):
+def getCountries():
+    allCountries = []
+    response = requests.get(URL+'countries')
+    data = response.json()
+    for country in data:
+        allCountries.append(country["Country"])
+    return allCountries
+
+def getData(country, status = 'confirmed'):
     countryInput = 'country/' + country.lower();
     if status == '1':
         status = 'confirmed'
@@ -16,40 +25,54 @@ def getData(country, status = 'confirmed', fromUTC = "2020-03-01T00:00:00Z", toU
         raise ValueError ('Value "'+ status + '" is invalid, please enter a number between 1 and 3')
 
 
-    status = '/status/' + status.lower()
+    status = 'status/' + status.lower()
     parameters = {
-        "from" : fromUTC,
-        "to" : toUTC,
     }
 
-    response = requests.get("https://api.covid19api.com/"+countryInput+status,parameters)
+    response = requests.get(URL+'dayone'+'/'+countryInput+'/'+status+'/'+'live',parameters)
     
     data = response.json()
     if type(data) == str:
         raise ValueError ('cannot find response, your value "' + country + '" is not a valid country' + APIreference)
     return data
+def searchProvinceCity(data):
+    geoType = 'City or Province'
+    ProvinceCity = set()
+    for datapoint in data:
+        if datapoint['City']:
+            ProvinceCity.add(datapoint['City'].lower())
+            geoType = 'City'
+        if datapoint['Province']:
+            ProvinceCity.add(datapoint['Province'].lower())
+            geoType = 'Province'
+    data = {}
+    data[geoType] = ProvinceCity
+    return data
 
-def parseData(data,province = '',city= ''):
+def parseData(data,geoType,provincecity = ''):
     days = []
     dates = []
     totalCases = []
     casesPerDay = []
     info = {}
     count = 0
-    info['country'] = data[0]['Country']
+    info['country'] = data[0]['Country'].capitalize()
+    info['provincecity'] = provincecity.capitalize()
     for subject in range(len(data)):
-        if (province.lower() == (data[subject])['Province'].lower()) and (city == (data[subject])['City'].lower()):
-            totalCases.append(data[subject]['Cases'])
-            count+=1
-            days.append(count)
-            dates.append(data[subject]['Date'])
-        if count == 0:
-            raise ValueError ( 'Your input of city or province is invalid, the database does not contain "'+ city + province + ' for ' + info['country'] + APIreference)
+        if provincecity.lower() in geoType['Province'] or provincecity.lower() in geoType['City']:
+            if provincecity.lower() == (data[subject])[(''.join(list(geoType.keys())))].lower():
+                totalCases.append(data[subject]['Cases'])
+                count+=1
+                days.append(count)
+                dates.append(data[subject]['Date'])
+        else:
+            raise ValueError ( 'Your input of either city or province is invalid, the database does not contain "'+ provincecity + '" for ' + info['country'] + APIreference)
     info['totalCases'] = totalCases
     info['days'] = days
     info['dates'] = dates
     info['type'] = (data[subject])['Status']
     info['day1'] = ''.join((data[0])['Date'].split())[:-10]
+
 
 
 
