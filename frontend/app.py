ec2401322f146a461977846531318ae2dc1406ee
Loriswen <junhuang1798@gmail.com>
2020-05-28 23:24:56 -0400

    wew

--- frontend/app.py
+++ frontend/app.py
@@ -1,3 +1,12 @@
+"""
+Simon Wang, 1830099
+Jun Hao Huang, 1830549
+May 28, 2020
+Vincent, instructor
+Final Project
+"""
+
+
 from frontend import *
 from getData import *
 from machinelearning import *
